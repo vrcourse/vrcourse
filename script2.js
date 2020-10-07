ca1ffe84d8851368a3a271ac128edc2b1dfa00fc
vrcourse <65391869+vrcourse@users.noreply.github.com>
2020-10-07 21:21:21 +0500

    commit07/10/2020-3

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
 $(function() {
-$(window).scroll(function() {
-if($(this).scrollTop() != 0) {
-$('#toTop').fadeIn();
-} else {
-$('#toTop').fadeOut();
-}
-});
-$('#toTop').click(function() {
-$('body,html').animate({scrollTop:0},800);
-});
+	$(window).scroll(function() {
+		if($(this).scrollTop() >= 800) {
+			$('#toTop').fadeIn();
+		} else {
+			$('#toTop').fadeOut();
+		}
+	});
+	$('#toTop').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});
 });
